f45f289c7df2bc84d172192daaa4ddd44a47513e
a13v3r <verbitskiy_al@mail.ru>
2024-02-20 20:16:19 +0300

    Physics, second lecture task, first version

---
+++ js/Tasks/Task2.js
@@ -0,0 +1,40 @@
+document.getElementById("build").addEventListener("click", buildGraphic);
+
+function buildGraphic() {
+    const radius = parseFloat(document.getElementById('radius').value);
+    const coilTurn = parseFloat(document.getElementById('n').value);
+    const currentValue = parseInt(document.getElementById('amperes').value);
+
+    const xs = [];
+    const ys = [];
+    const precision = 1000;
+
+    for (let x = -radius * 10; x <= radius * 10; x += radius / precision) {
+        xs.push(x);
+        ys.push(4 * Math.PI * Math.pow(10, -7) * coilTurn * currentValue * Math.pow(radius, 2) / 2
+            * (1 / Math.pow(Math.pow(Math.abs(x), 2)
+                    + Math.pow(Math.abs(radius), 2), 1.5) +
+                (1 / Math.pow(Math.pow(Math.abs(radius - x), 2)
+                    + Math.pow(Math.abs(radius), 2), 1.5))));
+    }
+
+    const points = [{x: xs, y: ys, mode: "lines"}];
+
+    Plotly.newPlot("answer", points,
+        {
+            title: "B(x)",
+            xaxis: {
+                title: 'x, м',
+            },
+            yaxis: {
+                title: 'В, Тл',
+            },
+            plot_bgcolor: '#363636',
+            paper_bgcolor: '#050505',
+            font: {
+                color: '#FFFFFF',
+                family: 'Deutsch Gothic',
+            },
+            colorway: ['#ee840c'],
+        });
+}
---
+++ js/header.js
@@ -0,0 +1,12 @@
+header.innerHTML = "<header>\n" +
+    "        <nav>\n" +
+    "\n" +
+    "            <div id=\"menu\">\n" +
+    "                <ul>\n" +
+    "                    <li class=\"active\"><a href=\"./index.html\">Home</a></li>\n" +
+    "                    <li><a href=\"#\">About Me</a></li>\n" +
+    "                    <li><a href=\"#\">GitHub</a></li>\n" +
+    "                </ul>\n" +
+    "            </div>\n" +
+    "        </nav>\n" +
+    "</header>";
