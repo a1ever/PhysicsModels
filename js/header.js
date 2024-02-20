ba3b17e16db12cd5428bc4eb01ecdb5a6fbe7da3
a13v3r <verbitskiy_al@mail.ru>
2024-02-20 20:36:44 +0300

    Physics, second lecture task, first version

--- js/header.js
+++ js/header.js
@@ -3,7 +3,7 @@ header.innerHTML = "<header>\n" +
     "\n" +
     "            <div id=\"menu\">\n" +
     "                <ul>\n" +
-    "                    <li class=\"active\"><a href=\"./index.html\">Home</a></li>\n" +
+    "                    <li class=\"active\"><a href=\"https://a1ever.github.io/ITMOPhysicsModels/\">Home</a></li>\n" +
     "                    <li><a href=\"#\">About Me</a></li>\n" +
     "                    <li><a href=\"#\">GitHub</a></li>\n" +
     "                </ul>\n" +
